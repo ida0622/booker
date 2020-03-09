8d6bd75dc4272a0124bbdc3287c2790b3133c0a2
ida tomoyoshi <tomo080622@gmail.com>
2020-03-09 17:08:08 +0900

    6-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+Refile.secret_key = 'b0013e4cbaac54e5127966c437e513d6c029012c3242e6ef784cb95baa9365872bfc5affba4efa8a2de65e6bb32893e55545a8633a63722a73eebdf2962871bb'
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
