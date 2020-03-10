8da2fa0c9ab5bed58be735e3bacccb6fed95f79d
ida tomoyoshi <tomo080622@gmail.com>
2020-03-10 18:52:58 +0900

    6-9

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_153642) do
+ActiveRecord::Schema.define(version: 2020_03_06_041856) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,11 +26,10 @@ ActiveRecord::Schema.define(version: 2020_03_08_153642) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_image_id"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
